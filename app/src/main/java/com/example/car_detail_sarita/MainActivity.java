197dd8267b68089e022e172d36d0a8afedfe66e7
Sarita Asthani <asthani77@outlook.com>
2019-04-13 11:53:59 +0545

    app_name changed, textview inside the scrollview added and field name corrected

--- app/src/main/java/com/example/car_detail_sarita/MainActivity.java
+++ app/src/main/java/com/example/car_detail_sarita/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.car_detail_sarita;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
@@ -6,22 +7,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
-private TextView txtScrollable;
-private EditText txtMake,txtYear,txtColor,txtPurchasePrice,txtEngineSize;
-private Button btnCreateCar;
+    private TextView txtScrollable;
+    private EditText txtMake, txtYear, txtColor, txtPurchasePrice, txtEngineSize;
+    private Button btnCreateCar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initItems();
     }
-    private void initItems(){
-        txtMake = findViewById(R.id.txtMake);
-        txtYear = findViewById(R.id.txtYear);
-        txtColor = findViewById(R.id.txtColor);
-        txtPurchasePrice = findViewById(R.id.txtPurchasePrice);
-        txtEngineSize = findViewById(R.id.txtEngineSize);
+
+    private void initItems() {
+        txtMake = findViewById(R.id.etMake);
+        txtYear = findViewById(R.id.etYear);
+        txtColor = findViewById(R.id.etColor);
+        txtPurchasePrice = findViewById(R.id.etPurchasePrice);
+        txtEngineSize = findViewById(R.id.etEngineSize);
 
         txtScrollable = findViewById(R.id.txtScrollable);
         txtScrollable.setMovementMethod(new ScrollingMovementMethod());
@@ -34,11 +38,13 @@ private Button btnCreateCar;
             }
         });
     }
-    private void performAction(){
-        String data = "\r\n" +"Make:"+txtMake.getText().toString()+"."+"\r\n"+ "Year:"+txtYear.getText().toString()
-                +"."+"\r\n"+ "Color:"+txtColor.getText().toString()+"."+"\r\n"
-                + "Purchase Price:"+txtPurchasePrice.getText().toString()
-                +"."+"\r\n"+ "Engine Size:"+txtEngineSize.getText().toString();
-            txtScrollable.setText(txtScrollable.getText().toString()+"\r\n"+data);
+
+    private void performAction() {
+        String data = "\r\n" + "Make: " + txtMake.getText().toString() + "." + "\r\n" + "Year:"
+                + txtYear.getText().toString()
+                + "." + "\r\n" + "Color: " + txtColor.getText().toString() + "." + "\r\n"
+                + "Purchase Price: " + txtPurchasePrice.getText().toString()
+                + "." + "\r\n" + "Engine Size: " + txtEngineSize.getText().toString();
+        txtScrollable.setText(txtScrollable.getText().toString() + "\r\n" + data);
     }
 }
